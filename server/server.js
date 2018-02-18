0cb3bae3da91717bdb0f2af89648c5711ed661a3
salt <boluuuu@gmail.com>
2018-02-18 15:45:17 +0100

    update for server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-let PORT = 8080;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
 // let IP = (process.env.OPENSHIFT_BUILD_NAME) ? "0.0.0.0"|| 'localhost';
 let path = require('path');
 let express = require('express');
 let app = express();
 let ExpressPeerServer = require('./peerServer/index').ExpressPeerServer;
-let server = app.listen(PORT, function () {
-    // console.log( "Listening on port " + PORT )
-});
+let server = app.listen(port, ip);
 
 
 app.use(express.static(path.join(__dirname + '/../src')));
